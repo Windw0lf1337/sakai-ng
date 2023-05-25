7d96167e1c1ba8b9288247da015961df7e0618e2
Windw0lf1337 <marcel.windolph@gmail.com>
2023-05-25 17:03:38 +0200

    .

--- src/app/demo/components/uikit/button/buttondemo.component.ts
+++ src/app/demo/components/uikit/button/buttondemo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import designTokens from '../../../../../assets/theme/cb-dark/design-token.json';
 
 @Component({
     templateUrl: './buttondemo.component.html'
@@ -17,12 +18,22 @@ export class ButtonDemoComponent implements OnInit {
             { label: 'Angular.io', icon: 'pi pi-info', url: 'http://angular.io' },
             { separator: true },
             { label: 'Setup', icon: 'pi pi-cog' }
-        ];
+        ];        
     }
 
     load(index: number) {
         this.loading[index] = true;
         setTimeout(() => this.loading[index] = false, 1000);
     }
-    
+
+    parse_json() {
+        console.log(designTokens);
+        var tokensLength = console.log(Object.keys(designTokens.global.button).length)
+        
+        for (let i = 0; i < tokensLength; i++) {
+            const element = array[i];
+            
+        }
+
+    }
 }
